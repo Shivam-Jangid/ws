72a13716d8e2fe575cf20063ce2c47d3f054b432
Shivam-Jangid <shivamjangid1125@gmail.com>
2025-12-18 19:10:04 +0530

    fixing bugs in room chat backend, added onclose functionaliy

--- backend/room-chat-backend/src/index.ts
+++ backend/room-chat-backend/src/index.ts
@@ -1,49 +1,49 @@
 import { WebSocket, WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({port:8080});
-
+// Maps roomName -> array of sockets
 const map = new Map<string, WebSocket[]>();
 
-wss.on("connection", (s)=>{
-    s.on("message", (e) => {
-        const mes = e.toString();
-        const mesJson = JSON.parse(mes);
+// Maps socket -> roomName
+const reverseMap = new Map();
 
-        if(mesJson.type == "join"){
-            const room = mesJson.room;
-            const sArray = map.get(room);
-            if(sArray){
-                sArray.push(s);
-                s.send("the length of the socket array is : "+ sArray.length);
-            }
-            else{
-                const newWebSocketArray:WebSocket[] = [];
-                newWebSocketArray.push(s);
-                map.set(room, newWebSocketArray);
-                const arr = map.get(room);
+wss.on("connection", (s) => {
 
-                s.send("the length of the newly created array is : "+arr?.length);
-            }
+    s.on("message", (data) => {
+        const { room } = JSON.parse(data.toString());
+
+        if (room) {
+            // add socket to map
+            const socketsArray = map.get(room) || [];
+            socketsArray.push(s);
+            map.set(room, socketsArray);
+
+            // track reverse mapping
+            reverseMap.set(s, room);
         }
+    });
 
-        else if(mesJson.type == "message"){
-            const room = mesJson.room;
-            const sarr = map.get(room);
-            if(sarr){
-                for(let i=0; i<sarr.length; i++){
-                    const currentSocket = sarr[i];
-                    if(currentSocket!=s){
-                        currentSocket.send(mesJson.message);
-                    }
+    s.on("close", () => {
+        const room = reverseMap.get(s);
+
+        if (room) {
+            let socketsArray = map.get(room);
+
+            if (socketsArray) {
+                // remove ALL occurrences of s
+                socketsArray = socketsArray.filter(socket => socket !== s);
+
+                if (socketsArray.length === 0) {
+                    // delete room if empty
+                    map.delete(room);
+                } else {
+                    // update room with filtered sockets
+                    map.set(room, socketsArray);
                 }
             }
 
-            else{
-                console.log(mesJson.message);
-                s.send("no joinees in this room");
-            }
+            // remove reverse mapping
+            reverseMap.delete(s);
         }
-
-    })
-})
-
+    });
+});
